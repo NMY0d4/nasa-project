6c791ec1202968aef30436dfef6b827071ab3de4
Grégory Marini <greg.marini@hotmail.fr>
2023-02-15 10:19:30 +0100

    fix bug

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const morgan = require("morgan");
-const api = require("./routes/api");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
   })
 );
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use("/v1", api);
+app.use('/v1', api);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public','index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = app;
